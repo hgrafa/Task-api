b75399fbf6f68407fc942df053141e8ce634c260
hgrafa <hugorafell@gmail.com>
2023-06-26 18:43:07 -0300

    updating model Path

--- src/utils/path.ts
+++ src/utils/path.ts
@@ -1,10 +1,22 @@
+import { buildRoutePath } from '../middlewares/build-route-path'
+
 export class Path {
   public url: string
+  public pathRegex: RegExp
   public params?: string[]
   public queryParams?: string[]
 
   private constructor(url: string) {
     this.url = url
+    this.pathRegex = buildRoutePath(url)
+  }
+
+  static from(...params: { key: string; value: string }[]) {
+    const paramsArray = params.map(({ key, value }) => {
+      return this.of(key, value)
+    })
+
+    return paramsArray
   }
 
   static of(...values: string[]): Path {
