740eb14c450f584ac531ed11747ba67eab3172e2
hgrafa <hugorafell@gmail.com>
2023-06-29 09:29:52 -0300

    adding POST, PATCH and DELETE routes

--- src/utils/route.ts
+++ src/utils/route.ts
@@ -3,7 +3,7 @@ import { Request } from './request'
 import { Path } from './path'
 
 export interface Route {
-  method: 'GET' | 'POST' | 'PUT' | 'DELETE'
+  method: 'GET' | 'POST' | 'PUT' | 'DELETE' | 'PATCH'
   path: Path
   handler: (req: Request, res: ServerResponse) => ServerResponse
 }
