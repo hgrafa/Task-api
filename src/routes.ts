59912ee8484d26aaacb1e6abffc8b8290734b634
hgrafa <hugorafell@gmail.com>
2023-06-29 09:27:51 -0300

    updating route methods

--- src/routes.ts
+++ src/routes.ts
@@ -1,7 +1,7 @@
-import { NewTask } from './domain/DTOs/taks-dtos'
+import { NewTask, TaskUpdate } from './domain/DTOs/taks-dtos'
 import { randomUUID } from 'crypto'
 import { Task } from './domain/model/task'
-import { TaskDatabase } from './repository'
+import { TaskDatabase } from './task-database'
 import { Path } from './utils/path'
 import { Route } from './utils/route'
 
@@ -35,6 +35,58 @@ export const routes: Route[] = [
 
       taskDatabase.insert(task)
       return res.writeHead(201).end()
+    },
+  },
+  {
+    method: 'PUT',
+    path: Path.of('tasks', ':id'),
+    handler: (req, res) => {
+      const { title, description }: TaskUpdate = req.body
+      const taskUpdate: TaskUpdate = { title, description }
+      // taskDatabase.update(taskUpdate)
+      return res.writeHead(201).end()
+    },
+  },
+  {
+    method: 'PATCH',
+    path: Path.of('tasks', ':id', 'complete'),
+    handler: (req, res) => {
+      if(!req.params?.has('id')) 
+        res.writeHead(406).end()
+        
+      const id = req.params?.get('id')
+        
+      if(!id)
+        res.writeHead(406).end()
+
+      let tasksFounded = taskDatabase.select((task) => {
+        return task.id === id
+      })
+
+        const task: Task = tasksFounded[0] ?? ({} as Task)
+        return res
+          .writeHead(task ? )
+          // TODO finish return restful status and content complemted
+
+      // taskDatabase.update(taskUpdate)
+      return res.writeHead(201).end()
+    },
+  },
+  {
+    method: 'DELETE',
+    path: Path.of('tasks', ':id'),
+    handler: (req, res) => {
+      const idParam = req.params?.find(({ key }) => {
+        return key === 'id'
+      })
+
+      let deleted = false
+
+      if (idParam) {
+        deleted = taskDatabase.delete(idParam.value)
+      }
+
+      return res.writeHead(deleted ? 204 : 406).end()
     },
   },
 ]
