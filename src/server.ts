2b1aed1c3f7437bee4d4467725e48b5dbcb2939d
hgrafa <hugorafell@gmail.com>
2023-06-29 09:30:18 -0300

    fixing the extrarion of params and query params at the main endpoint

--- src/server.ts
+++ src/server.ts
@@ -2,16 +2,34 @@ import http, { ServerResponse } from 'node:http'
 import { Request } from './utils/request'
 import { json } from './middlewares/json'
 import { routes } from './routes'
+import { extractQueryParams } from './middlewares/extract-query-params'
+import { Params } from './utils/params'
 
 const server = http.createServer(async (req: Request, res: ServerResponse) => {
-  const { method, url } = req
+  const { method, url = '' } = req
   await json(req, res)
 
   const route = routes.find((route) => {
-    return route.method === method && route.path.url === url
+    return route.method === method && route.path.pathRegex.test(url)
   })
 
   if (route) {
+    const routeParams = url.match(route.path.pathRegex) ?? { groups: {} }
+
+    Object.entries(routeParams.groups!).forEach(([key, value]) => {
+      if (key === 'query') {
+        req.queryParams = extractQueryParams(value ?? '')
+      } else {
+        if (!req.params) {
+          req.params = new Params()
+        }
+
+        req.params.add(key, value)
+      }
+    })
+
+    console.log(req.params)
+    console.log(req.queryParams)
     return route.handler(req, res)
   }
 
